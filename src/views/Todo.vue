7c48f035fa51cc62f2f8f0c3a120910928e70d46
dmitrykurmanov <kurmanov.work@gmail.com>
2017-04-04 22:32:03 +0300

    configure vue-waterfall

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        <waterfall :line-gap="200" :watch="items">
+        <waterfall
+                :line="line"
+                :line-gap="250"
+                :watch="items"
+                @reflowed="reflowed"
+                ref="waterfall"
+        >
             <!-- each component is wrapped by a waterfall slot -->
             <waterfall-slot
                     v-for="(item, index) in items"
                     :width="item.width"
                     :height="item.height"
                     :order="index"
-                    :key="item.id"
-                    :style="item.style"
+                    :key="item.index"
+                    move-class="item-move"
             >
-                <div>{{item.text}}</div>
+                <div class="item" :style="item.style" :index="item.index"></div>
             </waterfall-slot>
         </waterfall>
     </div>
@@ -21,6 +27,7 @@
     import {Component} from 'vue-property-decorator'
     import Waterfall from 'vue-waterfall/lib/waterfall'
     import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
+    import {ItemFactory} from './item-factory'
 
     @Component({
         components: {
@@ -29,37 +36,59 @@
         }
     })
     export default class Todo extends Vue {
-        items = [
-            {
-                id: 1,
-                width: 50,
-                height: 50,
-                style: {
-                    background: this.getRandomColor()
-                },
-                text: "1"
-            },
-            {
-                id: 2,
-                width: 20,
-                height: 20,
-                style: {
-                    background: this.getRandomColor()
-                },
-                text: "2"
-            }
-        ]
+        line = 'h'
+        items = ItemFactory.get(100)
+        isBusy = false
 
-        getRandomColor () {
-            var colors = [
-                'rgba(21,174,103,.5)',
-                'rgba(245,163,59,.5)',
-                'rgba(255,230,135,.5)',
-                'rgba(194,217,78,.5)',
-                'rgba(195,123,177,.5)',
-                'rgba(125,205,244,.5)'
-            ]
-            return colors[Math.floor(Math.random() * colors.length)]
+        addItems() {
+            if (!this.isBusy && this.items.length < 500) {
+                this.isBusy = true
+                this.items.push.apply(this.items, ItemFactory.get(50))
+            }
+        }
+        shuffle() {
+            this.items.sort(function () {
+                return Math.random() - 0.5
+            })
+        }
+        reflowed() {
+            this.isBusy = false
         }
     }
 </script>
+
+<style>
+    .item-move {
+        transition: all .5s cubic-bezier(.55,0,.1,1);
+        -webkit-transition: all .5s cubic-bezier(.55,0,.1,1);
+    }
+    body {
+        margin: 5px;
+        font-family: Helvetica, sans-serif;
+    }
+    .item {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        right: 5px;
+        bottom: 5px;
+        font-size: 1.2em;
+        color: rgb(0,158,107);
+    }
+    .item:after {
+        content: attr(index);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+    }
+    .wf-transition {
+        transition: opacity .3s ease;
+        -webkit-transition: opacity .3s ease;
+    }
+    .wf-enter {
+        opacity: 0;
+    }
+</style>
